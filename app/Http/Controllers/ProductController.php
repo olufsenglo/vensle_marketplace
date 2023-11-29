19786331f4fd085b085ed379c45d4374b78c4de2
John <johnolshea@gmail.com>
2023-11-29 11:20:10 +0100

    Fix validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -63,15 +63,21 @@ class ProductController extends Controller
 	    //Attach specifications: adds new entries to the pivot table
             $product->specifications()->attach($request->specification_ids);
 
-            return response()->json($product, 201);
+	    return response()->json($product, 201);
+	} catch (ValidationException $e) {
+		return response()->json(['errors' => $e->errors()], 422);
         } catch (\Exception $e) {
             Log::error('Error storing product: ' . $e->getMessage());
 	    
-	    if ($e instanceof \Illuminate\Database\QueryException) {
-	        return response()->json(['error' => 'Database error: ' . $e->getMessage()], 500);
-	    }
+	    /**
+	     * return response()->json(['error' => 'Internal Server Error'], 500);
+	     * TODO: create error handling middleware
+	     * ($e instanceof \Illuminate\Database\QueryException) {
+	     * return response()->json(['error' => 'Database error: ' . $e->getMessage()], 500);
+	     * }
+	     */
 
-            return response()->json(['error' => 'Internal Server Error'], 500);
+	    return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
@@ -126,13 +132,14 @@ class ProductController extends Controller
             $product->update($validatedData);
             $product->categories()->sync($request->category_ids);
 
-    	    // Sync specifications: update the pivot table for specifications
+    	    //Sync specifications: update the pivot table for specifications
             $product->specifications()->sync($request->specification_ids);
 
             return response()->json($product);
         } catch (\Exception $e) {
             Log::error('Error updating product: ' . $e->getMessage());
-            return response()->json(['error' => 'Internal Server Error'], 500);
+
+	    return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
